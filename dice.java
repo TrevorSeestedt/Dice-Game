c9142246091b377cdebcdd926971c7da3e1b2855
TrevorSeestedt <seestedt@email.sc.edu>
2024-06-03 19:37:06 -0400

    test

--- dice.java
+++ dice.java
@@ -1,2 +1,33 @@
-//this is a java test
+// Copyright of Trevor Seestedt 2024
+import java.util.Random; 
 
+public class Dice { 
+    private double choice; 
+    private String overUnder;   
+    public boolean result; 
+
+    //random number will be generated from 1 - 10000
+    Random rand = new Random();
+    double randomNum = (rand.nextInt(10000) / 100);
+
+    public boolean Game(double choice, String overUnder, boolean result) {
+        //if user chooses to go over 
+        if (overUnder.equalsIgnoreCase("Over")) {
+            if (choice > randomNum) {
+                result = true; 
+            }
+            else {
+                result = false; 
+            }
+        }
+        //if user chooses to go under 
+        else if (overUnder.equalsIgnoreCase("Under")) {
+            if (choice < randomNum) {
+                result =true; 
+            }
+            else {
+                result = false; 
+            }
+        }
+    }
+}
