97a236f3d29a86d654692937c2a588999d5bf328
TrevorSeestedt <seestedt@email.sc.edu>
2024-06-11 09:39:49 -0400

    test

--- BackEndUI.java
+++ BackEndUI.java
@@ -11,6 +11,7 @@ public class BackEndUI {
         System.out.println("\nWager must be more than 1 dollar.");
         System.out.print ("Wager: ");
         double wager = keyboard.nextDouble();
+        keyboard.nextLine();
         while (balance <= wager || wager < 1) {
             System.out.print ("Error. \n\nWager: ");
             wager = keyboard.nextDouble();
@@ -24,6 +25,7 @@ public class BackEndUI {
         System.out.println("Dice number must be between 0.01 and 100"); //implement slider in GUI
         System.out.print("\nEnter dice number: ");
         double diceChoice = keyboard.nextDouble();
+        keyboard.nextLine();
         while (diceChoice <= 0 || diceChoice > 100) {
             System.out.println("\nError! Dice choice is out of range. \n\nEnter dice number: ");                
             diceChoice = keyboard.nextDouble();
